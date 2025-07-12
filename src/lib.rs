64134ee1a85f17ef4cf9148482a394b85006594c
lovedeepsingh-07 <lovedeepsinghbrar07@gmail.com>
2025-07-12 09:53:38 +0530

    feat: add cargo dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,16 @@
 #[cxx::bridge(namespace = "kimyo")]
 mod ffi {
     struct Server {
-        address:  String,
         port: u16,
     }
 
     extern "Rust" {
-        fn start(self: &Server) -> String;
+        fn start(self: &Server);
     }
 }
 
 impl ffi::Server{
-    pub fn start(&self) -> String {
-        return format!("on the behalf of rust, I would like to say, you address is {:#?} and your port is {:#?}",self.address,self.port.to_string()).to_string();
+    pub fn start(&self) {
+        println!("Starting server...");
     }
 }
